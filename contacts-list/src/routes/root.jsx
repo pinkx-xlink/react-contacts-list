c714b263ffea257cd73db301eebbd4f899ca7694
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-25 15:34:39 -0500

    Import getContacts and createContact; create async function action()

--- contacts-list/src/routes/root.jsx
+++ contacts-list/src/routes/root.jsx
@@ -1,5 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { 
+    Outlet,
+    Link,
+    useLoaderData,
+    Form,
+} from "react-router-dom";
 import { getContacts } from "../contacts";
+import {getContacts, createContact } from "../contacts";
+
+export async function action() {
+    const contact = await createContact();
+    return { contact };
+}
 
 export async function loader() {
     const contacts = await getContacts();
@@ -7,6 +18,7 @@ export async function loader() {
 }
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
         <>
         <div id="sidebar">
@@ -34,6 +46,29 @@ export default function Root() {
                 </form>
             </div>
             <nav>
+                {contacts.length ? (
+                    <ul>
+                        {contacts.map((contact) => (
+                            <li key={contact.id}>
+                            <Link to={`contacts/${contacts.id}`}>
+                                {contact.first || contact.last ? (
+                                    <>
+                                        {contact.first} {contact.last}
+                                    </>
+                                ) : (
+                                    <i>No Name</i>
+                                )}{" "}
+                                {contact.favorite && <span>★</span>}
+                            </Link>
+                        </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>
+                        <i>No contacts</i>
+                    </p>
+                )}
+            </nav>
                 <ul>
                     <li>
                         <Link to={`/contacts/1`}>Your Name</Link>
@@ -42,7 +77,6 @@ export default function Root() {
                         <Link to={`/contacts/2`}>Your Friend</Link>
                     </li>
                 </ul>
-            </nav>
         </div>
         <div id="detail">
             <Outlet />
